aff81236cded3de171847b57a4e43d224e365336
Jonathan Carvalho Mendes <404217@inca.local>
2019-07-11 14:18:35 -0300

    first commit

---
+++ src/components/Curriculo.js
@@ -0,0 +1,255 @@
+import React, { Component } from 'react';
+
+
+export default class Curriculo extends Component {
+    render() {
+        return (
+            <section className="background3">
+                <div className="container">
+                    <div className="row adjust-cv-center">
+                    <div className="column fourth column1">
+                        <div>
+                            <h3 align="center" style={{ letterSpacing: "2px" }}>
+                                <b>JONATHAN <br />MENDES</b>
+                            </h3>
+                            <h6 align="center" style={{ letterSpacing: "2px" }}>Desenvolvedor Pleno</h6>
+                        </div>
+
+                        <div align="center" style={{ paddingTop: "55px" }}>
+                            <p className="text-title"> PERFIL</p>
+                            <hr />
+                            <p className="text-desc">
+                                <b>Tenho 30 anos,
+							    <br />
+							    <br />Ciência da Computação,
+							    <br />apaixonado por desenvolver.</b>
+                                <br />
+                            </p>
+                        </div>
+
+                        <div align="center" style={{ paddingTop: '40px' }}>
+                            <p className="text-title"> CONTATO </p>
+                            <hr />
+                            <p className="text-desc-2">
+                                <b>(11) 3985-5541</b>
+                                <br />
+                                <b>(11) 94904-0370</b>
+                                <br />
+                                <br />
+                                <b>Rua Pousada de Saramagos 174
+							<br />02473-000, Imirim
+							<br />São Paulo - SP</b>
+                                <br />
+                                <br />
+                                <b>www.victorlevi.com.br</b><br />
+                                <b>victorbatistalevi@gmail.com</b>
+                            </p>
+                        </div>
+
+                        <div align="center" style={{ paddingTop: "40px", paddingBottom: "40px" }}>
+                            <p className="text-title"> HABILIDADES </p>
+                            <hr />
+                            <div className="text-desc-2">
+                                <b> FRONT END</b>
+                                <br />
+                                <img src="https://image.ibb.co/i7fNsd/bar_front.png" alt="teste" />
+                                <br />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> BACK END</b>
+                                <br />
+                                <img src="https://image.ibb.co/cmGHQy/bar_back.png" />
+                            </div>
+                        </div>
+
+                        <div align="center" style={{ paddingTop: "40px", paddingBottom: "40px" }}>
+                            <p className="text-title"> coding </p>
+                            <hr />
+                            <div className="text-desc-2">
+                                <b> HTML / CCS</b>
+                                <br />
+                                <img src="https://image.ibb.co/gChDyJ/bar_html.png" />
+                                <br />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> JAVASCRIPT</b>
+                                <br />
+                                <img src="https://image.ibb.co/cbERJJ/bar_javascript.png" />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> SQL</b>
+                                <br />
+                                <img src="https://image.ibb.co/nxOxQy/bar_sql.png" />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> JAVA</b>
+                                <br />
+                                <img src="https://image.ibb.co/eAnP5y/bar_java.png" />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> REACTJS</b>
+                                <br />
+                                <img src="https://image.ibb.co/f672sd/bar_react.png" />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> REACT-NATIVE</b>
+                                <br />
+                                <img src="https://image.ibb.co/f672sd/bar_react.png" />
+                            </div>
+                            <div style={{ paddingTop: "20px" }}>
+                                <b> NODEJS</b>
+                                <br />
+                                <img src="https://image.ibb.co/n21j5y/bar_node.png" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="column two-thirds column2">
+                        <div align="left" style={{ paddingTop: "40px", paddingLeft: "30px" }}>
+                            <p className="text-title"> OBJETIVOS </p>
+                            <hr className="divider" />
+                            <b />
+                            <ul className="text-desc-2">
+                                <li>Desenvolvedor React-Native</li>
+                                <li>Atuar na área de desenvolvimento Front-End, Back-End.</li>
+                                <br />Desenvolvimento Mobile (React-Native, Ionic, Vue, etc.)
+										        <br />  <br />
+                                <li>Adquirir experiência profissional.</li>
+                                <li>Experiência no mercado de trabalho.</li>
+                            </ul>
+                        </div>
+
+                        <div align="left" style={{ paddingTop: "40px", paddingLeft: "30px" }}>
+                            <p className="text-title"> FORMAÇÃO </p>
+                            <hr className="divider" />
+                            <div>
+                                <p className="text-desc-3">
+                                    <b />• Pós-Graduação – Desenvolvimento Orientado
+                                    a Objetos com Java 
+								<br />
+                                </p>
+                                <p className="text-desc-5">Universidade Estácio de Sá 
+						            </p>
+                                <p className="text-desc-4">prevista para Dezembro de 2019.
+						            </p>
+                            </div>
+                            <div style={{ paddingTop: "30px" }}>
+                                <p className="text-desc-3">
+                                    <b />• Ensino Superior Completo – Gestão
+                                de Projetos Período 
+						</p>
+                                <p className="text-desc-5">
+                                Universidade Veiga de Almeida(UVA)
+						</p>
+                                <p className="text-desc-4">08/2015 – 11/2017</p>
+                            </div>
+                        </div>
+
+                        <div align="left" style={{ paddingTop: "40px", paddingLeft: "30px" }}>
+                            <p className="text-title"> Experiência de trabalho </p>
+                            <hr className="divider" />
+                            <div>
+                                <p className="text-desc-3">
+                                    <b />INCA - INSTITUTO NACIONAL DO CÂNCER
+								<br />
+                                </p>
+                                <p className="text-desc-5">Desenvolvedor Mobile Pleno
+							<br />
+                                </p>
+                                <p className="text-desc-4">Março 2019 - ATUAL
+							<br />
+                                    <br />Atualmente trabalho com Desenvolvimento sistemas Web baseados em
+                                   tecnologia de Back-End JAVA do cliente INCA - Instituto Nacional do Câncer -
+
+                                   <br />    <br />Atuo como Especialista Mobile Responsável pelo aplicativo INCA
+                                    utilizando IONIC 3+, com o uso extensivo de recursos Cordova e integração com
+                                    Serviços (http e Angular REST API ) , usando as melhores práticas do mercado
+                                    e recursos nativos de LocalNotifications, Geolocalização e Banco de dados
+                                    Local (LocalStorage, SQLLite).
+                                    <br />
+                                    <br />Projeto RHCWeb, BasePop e diversos legados usando Java 6 e 7 voltado para
+                                    Web, utilizando os frameworks:
+                                    <br /> • Hibernate  <br/>• JPA2 (Persistencia) <br /> • Spring (DI)  <br />• EJB 3.1  
+                                    <br />• Jboss 7  <br />• WebWork  <br />• Struts2 (MVC)  <br />• JSF2  <br />• Velocity  <br />• JSP(Views) 
+                                    <br />• XML Schemas  <br/>• Axis(Web Services)  <br />• JQuery(javascript), <br /> <br /> Bancos de dados  
+                                    <br />• Postgres  <br />• Oracle. <br />
+                                  
+						</p>
+                            </div>
+                        </div>
+
+                        <div align="left" style={{ paddingTop: "40px", paddingLeft: "30px" }}>
+                            <p className="text-title"> Conhecimentos </p>
+                            <hr className="divider" />
+                            <div className="text-desc-2">
+                                <b />
+                                <ul>
+                                    <li style={{ fontSize: "22px" }} className="color-text">Tecnologias Back-End:</li>
+                                    <ul>
+                                        <li>Java, C, SQL, SQL.</li>
+                                    </ul>
+                                </ul>
+
+                                <ul>
+                                    <li style={{ fontSize: "22px" }} className="color-text">Tecnologias Front-End: </li>
+                                    <ul>
+                                        <li>HTML com semântica padrões W3C, CSS3, JavaScript,
+										    <br /> Bootstrap, Material Design, Wordpress, MJML
+										    <br /> SquareSpace, FlexBox, Email-Marketing. </li>
+                                    </ul>
+                                </ul>
+
+                                <ul>
+                                    <li style={{ fontSize: "22px" }} className="color-text">Segurança da Inforamção:</li>
+                                    <ul>
+                                        <li>Pentest, SQLinjection, CyberSecutiry, Sniffing.</li>
+                                    </ul>
+                                </ul>
+
+                                <ul>
+                                    <li style={{ fontSize: "22px" }} className="color-text">Outras Tecnologias/Metodologias:</li>
+                                    <ul>
+                                        <li>Conhecimento no Pacote Office da Microsoft</li>
+                                        <li>Banco de Dados(modelagem)</li>
+                                        <li>Metodologia ágil (Scrum/Kanban), Git.</li>
+                                    </ul>
+                                </ul>
+
+                                <ul>
+                                    <li style={{ fontSize: "22px" }} className="color-text">Conhecimentos de Idiomas:</li>
+                                    <ul>
+                                        <li>Inglês Intermediário</li>
+                                        <li>Espanhol Técnico</li>
+                                    </ul>
+                                </ul>
+                            </div>
+                        </div>
+                        <div align="left" style={{ paddingTop: "40px", paddingLeft: "30px", paddingBottom: "50px" }}>
+                            <p className="text-title"> Cursos / Certificados </p>
+                            <hr className="divider" />
+                            <div className="text-desc-2">
+                                <b />
+                                <ul>
+                                    <li style={{ fontSize: "16px" }} className="color-text">Instituição/empresa: Alura</li>
+                                    <ul style={{ textTransform: "lowercase" }}>
+                                        <li>HTML e CSS3 I</li>
+                                        <li>HTML e CSS3 II</li>
+                                        <li>FLEXBOX</li>
+                                        <li>BOOTSTRAP</li>
+                                        <li>JAVASCRIPT BASICO</li>
+                                        <li>JAVASCRIPT AVANÇADO I</li>
+                                        <li>JAVASCRIPT AVANÇADO II</li>
+                                        <li>JAVASCRIPT AVANÇADO III</li>
+                                        <li>WEBPACK</li>
+                                        <li>REACTJS I</li>
+                                        <li>REACTJS II</li>
+                                    </ul>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </section>
+        );
+    }
+}
---
+++ src/components/Sobre.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Cell, Grid } from 'react-mdl';
+
+
+export default class Sobre extends Component {
+    render() {
+        return (
+
+            <section className="background2 ">
+                <div className="container">
+                    <div className="column full" id="sobre">
+                        <h1><span className="color-aquagreen2">Sobre</span></h1>
+                        <p>&lt;<span className="color-aquagreen2">p</span>&gt; Profissional com interesse eterno em aprender, em constante atualização com
+                            o mercado, em uma realidade altamente competitiva e comprometida com
+                            prazos com expectativas de clientes sei perfeitamente lidar com pressão e adoro
+                            de trabalhar com diversas tecnologias seja back-end ou front-end, sou pontual,
+                            tenho determinado, boa administração do tempo, espírito de liderança e
+                            facilidade de relacionamento interpessoal, bom humor, criativo e o principal
+                            apaixonado por tecnologia. &lt;/
+                        <span className="color-aquagreen2">p</span>&gt;</p>
+                    </div>
+                    
+                </div>
+                <br/>
+            </section>
+            
+        )
+    }
+}
